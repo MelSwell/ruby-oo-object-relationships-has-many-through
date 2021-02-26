3e3f98f30c26e033b08c91c5b6a684e479ca2233
Seth M Cohen <smcohen82@gmail.com>
2021-02-26 12:24:08 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,29 @@
+require 'pry'
 class Customer
+  attr_accessor :name, :age
   
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name
+    @ages = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip)
+    meal = Meal.new(waiter, customer = self, total, tip)
+  end
+
+  def meals
+    Meal.all.select { |meal| meal.customer.object_id == self.object_id }
+  end
+
+  def waiters
+    meals.collect { |meal| meal.waiter }
+  end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,28 @@
 class Waiter
+  attr_accessor :name, :years_of_experience
+  
+  @@all = []
+  
+  def initialize(name, years_of_experience)
+    @name = name
+    @years_of_experience = years_of_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip)
+    meal = Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    Meal.all.select { |meal| meal.waiter.object_id == self.object_id }
+  end
+
+  def best_tipper
+    meals.sort { |meal1, meal2| meal2.tip <=> meal1.tip }.first.customer
+  end
 
 end
